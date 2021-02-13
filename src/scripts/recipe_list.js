b8fab3e8a484f3c994ad1bfd68fcf0170d906c89
scglenn <samglennss3@gmail.com>
2021-02-13 13:50:20 -0600

    Added additional comments to recipe_list.js

--- src/scripts/recipe_list.js
+++ src/scripts/recipe_list.js
@@ -9,135 +9,136 @@
 
 */
 
+
+// Grab the number of recipes in local storage
 chrome.storage.sync.get('number_of_recipes', function(data) {
-    var number_of_recipes = data.number_of_recipes;
-    console.log("number_of_recipes:"+number_of_recipes);
     
+    // The number of recipes in local storage
+    var number_of_recipes = data.number_of_recipes;
+    
+    /*
+         _                 
+        | |                
+        | |__  _   _  __ _ 
+        | '_ \| | | |/ _` |
+        | |_) | |_| | (_| |
+        |_.__/ \__,_|\__, |
+                      __/ |
+                     |___/ 
+    */
     //There is a bug here where if we delete recipe 0 then everything breaks
     //Proposed solution: need to store a list of recipe ids instead of using number of recipes
     var recipe_list = [];
+
+    // Add each recipe id to the recipe list array
     for(var i = 0;i< number_of_recipes; i++)
     {
-        console.log("iteration:"+i);
+        
         var recipe_id_string = 'recipe_id_' + (i+1);
         recipe_list.push(recipe_id_string);
     }
 
     //Create html
-    function create(htmlStr) {
-        var frag = document.createDocumentFragment(),
-            temp = document.createElement('div');
+    function create(htmlStr) 
+    {
+        // The frag variable is a document fragment
+        // The temp variable is a div element
+        var frag = document.createDocumentFragment(),temp = document.createElement('div');
+
+        // Insert the html into the div element
         temp.innerHTML = htmlStr;
-        while (temp.firstChild) {
+
+        //This inserts the div element into the document fragment
+        while (temp.firstChild) 
+        {
             frag.appendChild(temp.firstChild);
-            }
-            return frag;
         }
 
-            //Variables cannot be used as keys without using computed keys, new in ES6.
-            chrome.storage.sync.get(recipe_list , function(data) 
+        // This returns formatted html
+        return frag;
+    }
+
+    // Get each recipe listed in the recipe list array from local storage
+    // Note: Variables cannot be used as keys without using computed keys, new in ES6.
+    chrome.storage.sync.get(recipe_list , function(data) 
+    {
+
+        // Iterate through each recipe
+        Object.keys(data).forEach(key => {
+
+            // Used as a temporary variable to index the dictionary
+            var temp_recipe_id_string = key;
+
+            // This log will display each key value pair in the recipe_list variable
+            console.log(key, data[key]);
+
+            // Create p tag that has the recipes name + edit/remove buttons
+            var fragment = create("<p>" + data[temp_recipe_id_string].recipe_name + "</p><button id='edit"+'_'+temp_recipe_id_string+"' type='button' class='btn btn-outline-primary'>Edit</button><button id='remove"+'_'+temp_recipe_id_string+"' type='button' class='btn btn-outline-secondary'>Remove</button>"); 
+    
+            // Insert the p tag into the document body
+            document.getElementById('recipeList').insertBefore(fragment, document.getElementById('recipeList').childNodes[0]);
+
+            // Access the edit button
+            var edit_btn = document.getElementById('edit'+'_'+temp_recipe_id_string);
+
+            // Access the remove button
+            var remove_btn = document.getElementById('remove'+'_'+temp_recipe_id_string);
+
+            //  This should fill the recipe page and send the user to recipe.html
+            edit_btn.onclick = function(element) 
             {
-                Object.keys(data).forEach(key => {
+                // Params are used in url as a way to transfer data between pages
+                window.location.href="../views/recipe.html?recipe_name="+data[temp_recipe_id_string].recipe_name+"&recipe_description="+data[temp_recipe_id_string].recipe_description+"&recipe_id="+temp_recipe_id_string+"";
+            };
 
-                    var temp_recipe_id_string = key;//recipe_id_string;
+            //  This should remove the recipe data and reload recipe_list.html
+            remove_btn.onclick = function(element) 
+            {
+                // Remove recipe and recipe data from local storage
+                chrome.storage.sync.remove([key], function(result) {
+                    
+                    // Get the number of recipes in local storage
+                    // NOTE: This should probably be chaned because this is the second time its called
+                    chrome.storage.sync.get('number_of_recipes', function(data) {
 
-                    console.log(key, data[key]);
-
-                    var fragment = create("<p>" + data[temp_recipe_id_string].recipe_name + "</p><button id='edit"+'_'+temp_recipe_id_string+"' type='button' class='btn btn-outline-primary'>Edit</button><button id='remove"+'_'+temp_recipe_id_string+"' type='button' class='btn btn-outline-secondary'>Remove</button>"); 
-            
-                document.getElementById('recipeList').insertBefore(fragment, document.getElementById('recipeList').childNodes[0]);
-                //document.body.insertBefore(fragment, /*document.body.childNodes[0]*/ document.getElementById('recipeList'));
-
-                var edit_btn = document.getElementById('edit'+'_'+temp_recipe_id_string);
-                var remove_btn = document.getElementById('remove'+'_'+temp_recipe_id_string);
-
-                //Design: 
-                //  This should fill the recipe page and send the user to recipe.html
-                edit_btn.onclick = function(element) 
-                {
-                    window.location.href="../views/recipe.html?recipe_name="+data[temp_recipe_id_string].recipe_name+"&recipe_description="+data[temp_recipe_id_string].recipe_description+"&recipe_id="+temp_recipe_id_string+"";
-                    //.search   → '?filter=JS'
-                    //Problem: I need to fill recipe.html
-                };
-
-                //Design:
-                //  This should remove the recipe data and reload recipe_list.html
-                remove_btn.onclick = function(element) 
-                {
-                    chrome.storage.sync.remove([key], function(result) {
-                        console.log('key being removed currently is ' + result.key);
-                      });
-                      chrome.storage.sync.get('number_of_recipes', function(data) {
+                        // Reduce number of recipes by 1
                         var num_of_recipes = data.number_of_recipes-1;
                 
-                        chrome.storage.sync.set({'number_of_recipes' : num_of_recipes}, function() {
-                            // Notify that we saved.
-                            //message('Settings saved');
+                        // Set the new number of recipes in local storage
+                        chrome.storage.sync.set({'number_of_recipes' : num_of_recipes}, 
+                        function() {
+                            
+
                         });
                     });
-                    window.location.href="../views/recipe_list.html";
-                };
 
-                  });
+                    // Transition user to recipe list page
+                    window.location.href="../views/recipe_list.html";
+                });
+            };
         });
+    });
 });
 
-
+// Access the add button
 var add_btn = document.getElementById('add');
+
+// Access the back button
 var back_btn = document.getElementById('back');
 
-//Design:
 //  This should send the user to a blank recipe.html
 add_btn.onclick = function(element) 
 {
     window.location.href="../views/recipe.html?recipe_name="+"test"+"&recipe_description="+"test"+"&recipe_id="+undefined+"";
-    //Potential Solution: I may be able to pass data this way
-    //Problem: I need to go to a blank recipe.html
 };
 
+// This should send the user to the default popup page
 back_btn.onclick = function(element) 
 {
     window.location.href="../views/popup.html"
 };
 
-
-
-
-
-// let changeColor = document.getElementById('changeColor');
-
-// chrome.storage.sync.get('color', function(data) {
-//   changeColor.style.backgroundColor = data.color;
-//   changeColor.setAttribute('value', data.color);
-// });
-
-// function onExecuted(result) {
-//     console.log(`We made it a color`);
-// };
-
-// function popup() {
-//     chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
-//     var activeTab = tabs[0];
-//     chrome.tabs.sendMessage(activeTab.id, { doSkip: true })
-//    });
-// }
-
-//  changeColor.onclick = function(element) {
-//   let color = element.target.value;
-//   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//         chrome.storage.sync.set({color: 'red'}, function() {
-//             changeColor.style.backgroundColor = 'red'
-//             changeColor.setAttribute('value', 'red');
-//         });
-      
-//         chrome.tabs.executeScript( null, 
-//         {code:"document.body.style.backgroundColor = 'red';" },
-//         function(results){ chrome.extension.getBackgroundPage().console.log(results); } );
-    
-//         popup();
-//     });
-// };
-
+// Listener for message from extension or content script
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       console.log(sender.tab ?
@@ -148,19 +149,4 @@ chrome.runtime.onMessage.addListener(
     var test= document.getElementById('recipe');
     test.textContent = request.food_list;
     }
-  );
-
-
-//   // To permanently change a popup while it is closed:
-
-//   // chrome.browserAction.setPopup({popup: "new.html"});
-//   // If you want to temporary change a popup while it is still open:
-  
-//   // window.location.href="new.html";
-// show_ingredients_btn.onclick = function(element) {
-//   window.location.href="ingredients.html"
-// };
-
-// show_recipes_btn.onclick = function(element) {
-//   window.location.href="recipes.html"
-// };
+);
